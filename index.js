765154ccfef601644a660fdea6ca04739bbc98bb
Fatima <fatimarezaie705@gmail.com>
2023-09-10 04:10:05 -0700

    crm.elitevallyProject home page and blog page

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const d = new Date();
+
+document.getElementById('date').innerHTML = d.getDate()+'/'+(d.getMonth() + 1)+'/'+d.getFullYear();
+document.getElementById('date-1').innerHTML = d.getDay()+'/'+(d.getMonth() + 1)+'/'+d.getFullYear();
+document.getElementById('date-2').innerHTML = d.getDay()+'/'+(d.getMonth() + 1)+'/'+d.getFullYear();
+document.getElementById('date-3').innerHTML = d.getDay()+'/'+(d.getMonth() + 1)+'/'+d.getFullYear();
+
+
